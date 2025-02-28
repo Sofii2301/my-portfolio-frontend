0e8739b2731e62b56222dd6be9dfe35731a1dd94
Sofii2301 <sofialvarezrodero97@gmail.com>
2025-02-28 14:26:46 -0300

    Initial commit - starting to create the navbar and home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import background from '../assets/background_home.jpg'
+
+const Home = () => {
+  return (
+    <div className='w-100 h-100'>
+        <img src={background} alt="" />
+        <div className='row'>
+            <div className='col'>
+                <h1>Hi there,I'm Sofia Alvarez Rodero</h1>
+                <h3></h3>
+            </div>
+            <div className='col'>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
