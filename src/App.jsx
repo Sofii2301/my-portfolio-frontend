0798c83bded3d3007b3e854cfd0cc44ffdb3183f
Sofii2301 <sofialvarezrodero97@gmail.com>
2025-03-02 20:45:20 -0300

    Finish navbar and main layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Experience from "./pages/Experience";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 
+import './App.css'
+import './assets/css/home.css'
+
 export default function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/experience" element={<Experience />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </MainLayout>
     </Router>
   );
 }
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,26 @@
+import Navbar from "../components/organisms/Navbar";
+import "../assets/css/mainLayout.css";
+import backgroundImage from "../assets/background_home.jpg";
+
+export default function MainLayout({ children }) {
+  return (
+    <div 
+        className="layout-container" 
+        style={{ 
+            backgroundImage: `url(${backgroundImage})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+        }}
+    >
+        {/* Background with Overlay (for the ENTIRE page) */}
+        <div className="background-overlay"></div>
+
+        {/* Navbar */}
+        <Navbar />
+
+        {/* Page content */}
+        <main>{children}</main>
+    </div>
+  );
+}
