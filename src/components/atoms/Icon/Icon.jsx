2e76eebb43ff6c3756b8cc1cc3e3679e9ea4c9bf
Sofii2301 <sofialvarezrodero97@gmail.com>
2025-04-10 19:12:31 -0300

    Fix Icon

--- src/components/atoms/Icon/Icon.jsx
+++ src/components/atoms/Icon/Icon.jsx
@@ -3,21 +3,15 @@ import { Link } from "react-router-dom";
 import "./icon.css";
 
 export default function Icon({ name, link, text }) {
-  const content = (
-    <>
-      <i className={`bi bi-${name}`}></i>
-      <p>{text}</p>
-    </>
-  );
-  
   return link ? (
     <Link to={link} target="_blank" className="icon icon-with-link" >
       <i className={`bi bi-${name}`}></i>
-      <p>{text}</p>
+      {text && <p>{text}</p>}
     </Link>
   ) : (
     <div className="icon">
-      {content}
+      <i className={`bi bi-${name}`}></i>
+      {text && <p>{text}</p>}
     </div>
   );
 }
