1762233a752f368552b48ecf12edb1dadcfaa218
Sofii2301 <sofialvarezrodero97@gmail.com>
2025-04-16 03:19:23 -0300

    fix icons row

--- src/components/molecules/SocialIcons/SocialIcons.jsx
+++ src/components/molecules/SocialIcons/SocialIcons.jsx
@@ -4,7 +4,7 @@ import './socialIcons.css'
 export default function SocialIcons() {
     return (
         <>
-            <div className="row social-icons">
+            <div className="social-icons">
                 <Icon name="instagram" link="https://www.instagram.com/sofi_alvarez.32/" />
                 <Icon name="tiktok" link="https://www.tiktok.com/@sofialvarez44" />
                 <Icon name="linkedin" link="https://www.linkedin.com/in/sofi-alvarez-rodero/" />
